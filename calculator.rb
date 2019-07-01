adf166d5f8f9440f6ef5c704828e8b44c8b81a42
Martin Crabtree <martin.crabtree@gmail.com>
2019-07-01 20:28:41 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,9 +1,10 @@
 # Add your variables here
 def addition(first_number, second_number)
-  sum
+  sum = first_number + second_number
 end
 
 
 first_number = 12
 second_number = 4
 
+addition(first_number, second_number)
