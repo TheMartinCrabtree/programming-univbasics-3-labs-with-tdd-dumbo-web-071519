2a0b5a92cc046ec99408ad3e894b5fd583a3b320
Martin Crabtree <martin.crabtree@gmail.com>
2019-07-01 20:34:36 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -7,9 +7,19 @@ def subtraction(first_number, second_number)
   first_number - second_number
 end
 
+def multiply(first_number, second_number)
+  first_number * second_number
+end
+
+def divide(first_number, second_number)
+  first_number / second_number
+end
+
 
 first_number = 12
 second_number = 4
 
 sum = addition(first_number, second_number)
 difference = subtraction(first_number, second_number)
+product = 
+quotient = 
