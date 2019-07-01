ff9b02d8801ddb108d5ea253f573a1104240f7c8
Martin Crabtree <martin.crabtree@gmail.com>
2019-07-01 20:30:57 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -3,8 +3,13 @@ def addition(first_number, second_number)
   sum = first_number + second_number
 end
 
+def subtraction(first_number, second_number)
+  difference = first_number - second_number
+end
+
 
 first_number = 12
 second_number = 4
 
 sum = addition(first_number, second_number)
+difference =
