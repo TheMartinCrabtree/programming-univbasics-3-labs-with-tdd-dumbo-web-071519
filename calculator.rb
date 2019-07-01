5af6eb956991d8a6f750a43b3840aed5908b9506
Martin Crabtree <martin.crabtree@gmail.com>
2019-07-01 20:32:39 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,10 +1,10 @@
 # Add your variables here
 def addition(first_number, second_number)
-  sum = first_number + second_number
+  first_number + second_number
 end
 
 def subtraction(first_number, second_number)
-  difference = first_number - second_number
+  first_number - second_number
 end
 
 
@@ -12,4 +12,4 @@ first_number = 12
 second_number = 4
 
 sum = addition(first_number, second_number)
-difference =
+difference = subtraction(first_number, second_number)
